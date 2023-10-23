60f9a7dad772c5354a00a57ecbc60f9aad000f1d
BiahMarqyes <santosmarquesbeatriz@gmail.com>
2023-10-23 11:46:12 -0300

    auth.service fdp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,26 @@ import { AppComponent } from './app.component';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ReactiveFormsModule, FormsModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ReactiveFormsModule, FormsModule,
+            AngularFireModule.initializeApp(
+              {
+                apiKey: "AIzaSyDpr46AI1XNYjuNLeT-gaXymHi8Rjm6lm4",
+                authDomain: "tecece-9938a.firebaseapp.com",
+                projectId: "tecece-9938a",
+                storageBucket: "tecece-9938a.appspot.com",
+                messagingSenderId: "946856601170",
+                appId: "1:946856601170:web:395ebb2af58a3d287de18b",
+                measurementId: "G-84YX78PSNB"
+              }
+            ), AngularFireAuthModule, AngularFireDatabaseModule
+           ],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
